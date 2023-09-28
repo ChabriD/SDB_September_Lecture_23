5640749e67902df04aa783f08ea34fcb8c80fc88
Chabri <cmd.18@icloud.com>
2023-09-27 17:41:38 -0700

    finished operator lesson

---
+++ Unit_2/Variables&Console/operators.js
@@ -0,0 +1,64 @@
+/*! Operators
+    These are what help us manipulate our data so that we can update, increase, separate, or simply change it all together.
+    - Plus        +
+    - Times       *
+    - Power       **
+    - Dot         .
+    - Assignment  =
+    - Comparison  ==
+    
+    Used to:
+        - Assign
+        - Compare
+        - Arithmetic
+        - Logical
+        - Conditional
+
+    Expressions & Statement: 
+    
+    keyword name (expression) {
+        code block that runs
+    }
+*/
+
+/* 
+    Comparison Operators
+    -JS does coercion when comparing values
+        Coercion: process of converting one value type to another
+ */
+
+// Equal to ==
+
+console.log("3" == 3);
+
+/*Strict Equal to, ===
+    checks to see if values match
+*/
+
+// Greater Than
+3 > 2;
+
+// Less Than
+4 < 2;
+
+/*  Greater than or equal to
+    - greater than sign needs to come before the equal sign in order to work
+ */
+
+console.log(2 >= 2);
+
+// And, &&
+2 && 3;
+
+// or, ||
+2 || 3;
+
+/* 
+Logical Operators
+    -Arithmetic Operators
+ */
+
+// Addition Operator
+ r += 1; // r = r + 1;
+
+//
